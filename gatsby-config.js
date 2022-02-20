959be6d7e3c133490c6a9263957655ffb4ffedef
Pero <ynwangpeng@gmail.com>
2022-02-20 10:23:53 +0800

    gatsby-plugin-google-gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,10 +54,11 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: `G-J6DFBN2W2Q`,
-        head: true,
+        trackingIds: [
+          `G-J6DFBN2W2Q`,
+        ],
       },
     },
     {
